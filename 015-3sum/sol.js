4d38441abd9675405b1285f711aad8af9f81f2ac
syaning <syaningv@gmail.com>
2016-07-15 20:35:12 +0800

    update 015

--- 015-3sum/sol.js
+++ 015-3sum/sol.js
@@ -3,15 +3,40 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-	var set = new Set(nums)
+	nums.sort((a, b) => a - b)
 	var result = []
+
 	for (let i = 0; i < nums.length; i++) {
-		for (let j = i + 1; j < nums.length; j++) {
-			let sum = -nums[i] - nums[j]
-			if (set.has(sum)) {
-				result.push([nums[i], nums[j], -sum])
+		if (i >= 1 && nums[i] === nums[i - 1]) {
+			continue
+		}
+		let left = i + 1
+		let right = nums.length - 1
+		while (left < right) {
+			let sum = nums[left] + nums[right] + nums[i]
+			if (sum === 0) {
+				result.push([nums[i], nums[left], nums[right]])
+				left++
+				right--
+				while (left < right && nums[left] === nums[left - 1]) {
+					left++
+				}
+				while (left < right && nums[right] === nums[right + 1]) {
+					right--
+				}
+			} else if (sum < 0) {
+				left++
+				while (left < right && nums[left] === nums[left - 1]) {
+					left++
+				}
+			} else {
+				right--
+				while (left < right && nums[right] === nums[right + 1]) {
+					right--
+				}
 			}
 		}
 	}
+
 	return result
 }
