05355e75078bec957fec7f55288a2c285d68180b
syaning <syaningv@gmail.com>
2016-07-17 17:47:24 +0800

    update sol

--- 036-valid-sudoku/sol.js
+++ 036-valid-sudoku/sol.js
@@ -3,39 +3,37 @@
  * @return {boolean}
  */
 var isValidSudoku = function(board) {
-	function valid(nums) {
-		nums = nums.filter(x => x !== '.');
-		return new Set(nums).size === nums.length;
-	}
+  function valid(nums) {
+    nums = nums.filter(x => x !== '.')
+    return new Set(nums).size === nums.length
+  }
 
-	var i, jnums;
+  for (let i = 0; i < 9; i++) {
+    if (!valid(board[i])) {
+      return false
+    }
+  }
 
-	for (i = 0; i < 9; i++) {
-		if (!valid(board[i])) {
-			return false;
-		}
-	}
+  for (let i = 0; i < 9; i++) {
+    let nums = []
+    for (let j = 0; j < 9; j++) {
+      nums.push(board[j][i])
+    }
+    if (!valid(nums)) {
+      return false
+    }
+  }
 
-	for (i = 0; i < 9; i++) {
-		nums = [];
-		for (j = 0; j < 9; j++) {
-			nums.push(board[j][i]);
-		}
-		if (!valid(nums)) {
-			return false;
-		}
-	}
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      let nums = board[j * 3].slice(i * 3, i * 3 + 3)
+        .concat(board[j * 3 + 1].slice(i * 3, i * 3 + 3))
+        .concat(board[j * 3 + 2].slice(i * 3, i * 3 + 3))
+      if (!valid(nums)) {
+        return false
+      }
+    }
+  }
 
-	for (i = 0; i < 3; i++) {
-		for (j = 0; j < 3; j++) {
-			nums = board[j * 3].slice(i * 3, i * 3 + 3)
-				.concat(board[j * 3 + 1].slice(i * 3, i * 3 + 3))
-				.concat(board[j * 3 + 2].slice(i * 3, i * 3 + 3));
-			if (!valid(nums)) {
-				return false;
-			}
-		}
-	}
-
-	return true;
-};
+  return true
+}
