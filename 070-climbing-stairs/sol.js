da7f698120fe03a7828703b2b34a1a47a4984190
syaning <syaningv@gmail.com>
2016-07-23 22:22:11 +0800

    update 070

--- 070-climbing-stairs/sol.js
+++ 070-climbing-stairs/sol.js
@@ -3,11 +3,13 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-	var prev = 0;
-	var current = 1;
-	for (var i = 0; i < n; i++) {
-		current = prev + current;
-		prev = current - prev;
-	}
-	return current;
-};
+  var prev = 0
+  var current = 1
+
+  for (let i = 0; i < n; i++) {
+    current = prev + current
+    prev = current - prev
+  }
+
+  return current
+}
