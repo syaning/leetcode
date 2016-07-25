eb4305a7a3a131515c1b31cdc1b964f2b0385d1e
syaning <syaningv@gmail.com>
2016-07-25 17:47:00 +0800

    update 100

--- 100-same-tree/sol.py
+++ 100-same-tree/sol.py
@@ -1,11 +1,13 @@
 # Definition for a binary tree node.
-class TreeNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 
 class Solution(object):
+
     def isSameTree(self, p, q):
         """
         :type p: TreeNode
@@ -14,8 +16,6 @@ class Solution(object):
         """
         if not p and not q:
             return True
-
         if not p or not q:
             return False
-
         return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
