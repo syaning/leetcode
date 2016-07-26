f04a40536f816ab55898304b926993d5b462c3f3
syaning <syaningv@gmail.com>
2016-07-26 21:38:39 +0800

    add 107

---
+++ 107-binary-tree-level-order-traversal-2/sol.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrderBottom = function(root) {
+  if (!root) {
+    return []
+  }
+
+  var current = [root]
+  var result = []
+
+  while (current.length) {
+    let vals = []
+    let next = []
+
+    for (let node of current) {
+      vals.push(node.val)
+      if (node.left) {
+        next.push(node.left)
+      }
+      if (node.right) {
+        next.push(node.right)
+      }
+    }
+
+    result.push(vals)
+    current = next
+  }
+
+  return result.reverse()
+}
--- README.md
+++ README.md
@@ -106,7 +106,7 @@
 104. [Maximum Depth of Binary Tree](./104-maximum-depth-of-binary-tree)
 105. Construct Binary Tree from Preorder and Inorder Traversal
 106. Construct Binary Tree from Inorder and Postorder Traversal
-107. Binary Tree Level Order Traversal II
+107. [Binary Tree Level Order Traversal II](./107-binary-tree-level-order-traversal-2)
 108. Convert Sorted Array to Binary Search Tree
 109. Convert Sorted List to Binary Search Tree
 110. Balanced Binary Tree
