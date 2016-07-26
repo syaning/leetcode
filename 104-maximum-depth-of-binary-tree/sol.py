959597c798ac3f08e6c84350112e4f654b2791c0
syaning <syaningv@gmail.com>
2016-07-26 16:15:51 +0800

    update 104

--- 104-maximum-depth-of-binary-tree/sol.py
+++ 104-maximum-depth-of-binary-tree/sol.py
@@ -1,12 +1,13 @@
 # Definition for a binary tree node.
-class TreeNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 
 class Solution(object):
+
     def maxDepth(self, root):
         """
         :type root: TreeNode
