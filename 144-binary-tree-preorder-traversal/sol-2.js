830d6d875c176ad7e7d0c2538ebafa8da643a3f3
syaning <syaningv@gmail.com>
2016-07-26 12:04:35 +0800

    add 144

---
+++ 144-binary-tree-preorder-traversal/sol-2.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+    if (!root) {
+        return []
+    }
+
+    var result = []
+    var stack = [root]
+
+    while (stack.length) {
+        let node = stack.pop()
+        result.push(node.val)
+        if (node.right) {
+            stack.push(node.right)
+        }
+        if (node.left) {
+            stack.push(node.left)
+        }
+    }
+
+    return result
+}
